338a77d50ffa7dcdeefe1ded0c19384f89f6cef7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-09-21 06:54:04 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Home from "./componentes/telas/Home";
 import Menu from "./componentes/telas/Menu";
-import Categoria from "./componentes/telas/categoria/Categoria";
-import Produto from "./componentes/telas/produto/Produto";
+import BadLanguage from "./componentes/telas/badLanguage/BadLanguage";
+import Alert from "./componentes/telas/alert/Alert";
+import MaliciousWebsite from "./componentes/telas/maliciousWebsite/MaliciousWebsite";
+import MaliciousProcess from "./componentes/telas/maliciousProcess/MaliciousProcess";
+import MaliciousPort from "./componentes/telas/maliciousPort/MaliciousPort";
 
 const router = createBrowserRouter([
   {
