f0bed876e1453f9771197af12488df861b5b9600
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-09-21 22:24:47 -0300

    Update Tabela.jsx

--- src/componentes/telas/maliciousPort/Tabela.jsx
+++ src/componentes/telas/maliciousPort/Tabela.jsx
@@ -21,8 +21,8 @@ function Tabela() {
                     <thead>
                         <tr>
                             <th scope="col" style={{ textAlign: 'center' }}>Ações</th>
-                            <th scope="col">Código</th>
-                            <th scope="col">Nome</th>
+                            <th scope="col">Id</th>
+                            <th scope="col">vulnarableBanners</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -30,17 +30,17 @@ function Tabela() {
                             <tr key={objeto.codigo}>
                                 <td align="center">
                                     <button className="btn btn-info"
-                                        onClick={() => editarObjeto(objeto.codigo)}
+                                        onClick={() => editarObjeto(objeto.id)}
                                         data-bs-toggle="modal" data-bs-target="#modalEdicao">
                                         <i className="bi bi-pencil-square"></i>
                                     </button>
                                     <button className="btn btn-danger" title="Remover"
-                                        onClick={() => { remover(objeto.codigo); }}>
+                                        onClick={() => { remover(objeto.id); }}>
                                         <i className="bi bi-trash"></i>
                                     </button>
                                 </td>
-                                <td>{objeto.codigo}</td>
-                                <td>{objeto.nome}</td>
+                                <td>{objeto.id}</td>
+                                <td>{objeto.vulnarableBanners}</td>
                             </tr>
                         ))}
                     </tbody>
