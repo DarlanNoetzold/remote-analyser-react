a6d5dfd8755a28bed6e3807705dcf6a41cf9f3e9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-04 22:52:14 -0300

    Create AlertUser.jsx

---
+++ src/componentes/telas/alertUser/AlertUser.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import Tabela from "./Tabela";
+import Carregando from "../../comuns/Carregando";
+import WithAuth from "../../../seguranca/WithAuth";
+import { useNavigate } from "react-router-dom";
+
+
+import {
+  addAlertAPI,
+  getAlertByPcIdAPI,
+  removeAlertByIdAPI,
+} from "../../../servicos/services";
+import AlertUserContext from "./AlertUserContext";
+
+function Alert() {
+
+  let navigate = useNavigate();
+
+  const [alerta, setAlerta] = useState({ status: "", message: "" });
+  const [listaObjetos, setListaObjetos] = useState([]);
+  const [editar, setEditar] = useState(false);
+  const [objeto, setObjeto] = useState({
+    pcId: "",
+    image: {
+      id: 0,
+      productImg: "",
+      base64Img: "",
+    },
+    processos: null,
+    dataCadastro: "",
+  });
+  const [carregando, setCarregando] = useState(false);
+
+  const acaoCadastrar = async (e) => {
+    e.preventDefault();
+    if (!objeto.pcId) {
+      setAlerta({ status: "Error", message: "pcId deve ser preenchido" });
+      return;
+    }
+    const metodo = editar ? "PUT" : "POST";
+    try {
+      let retornoAPI = await addAlertAPI(objeto, metodo);
+      setAlerta({ status: "Created", message: retornoAPI.pcId });
+      setObjeto(retornoAPI);
+      if (!editar) {
+        setEditar(true);
+      }
+    } catch (err) {
+      window.location.reload();
+      navigate("/login", { replace: true });
+    }
+
+    recuperaAlerts();
+  };
+
+  const recuperaAlerts = async () => {
+    setCarregando(true);
+    const localStorageAutenticacao = localStorage.getItem('remote/autenticacao');
+    const autenticacao = localStorageAutenticacao ?
+        JSON.parse(localStorageAutenticacao) : null;
+    const alerts = await getAlertByPcIdAPI(autenticacao.username);
+    setListaObjetos(alerts);
+    console.log(listaObjetos);
+    setCarregando(false);
+  };
+
+  const remover = async (codigo) => {
+    try{
+      if (window.confirm("Deseja remover este objeto")) {
+        let retornoAPI = await removeAlertByIdAPI(codigo);
+        setAlerta({ status: retornoAPI.status, message: retornoAPI.message });
+        recuperaAlerts();
+      }
+    } catch (err) {
+      window.location.reload();
+      navigate("/login", { replace: true });
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setObjeto({...objeto , [name] : value});
+}
+
+  const handleChangeImg = (e) => {
+    const { name, value } = e.target;
+    if(name === "ImageId"){
+      setObjeto({
+        ...objeto,
+        image:{
+          id: value
+        }
+      })
+    }    
+  };
+
+  useEffect(() => {
+    recuperaAlerts();
+  }, []);
+
+  return (
+    <AlertUserContext.Provider
+      value={{
+        alerta,
+        listaObjetos,
+        remover,
+        objeto,
+        editar,
+        acaoCadastrar,
+        handleChange,
+        handleChangeImg
+      }}
+    >
+      <Carregando carregando={carregando}>
+        <Tabela />
+      </Carregando>
+    </AlertUserContext.Provider>
+  );
+}
+
+export default WithAuth(Alert);
