d7c4bd840582edb706a09d6075688d7f4145af7c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-05 17:01:36 -0300

    Update Tabela.jsx

--- src/componentes/telas/maliciousWebsite/Tabela.jsx
+++ src/componentes/telas/maliciousWebsite/Tabela.jsx
@@ -4,7 +4,7 @@ import Alerta from '../../comuns/Alerta';
 
 function Tabela() {
 
-    const { alerta, listaObjetos, remover, novoObjeto, editarObjeto } = useContext(MaliciousWebsiteContext);
+    const { alerta, listaObjetos, remover, novoObjeto, editarObjeto, previousPage, nextPage, page } = useContext(MaliciousWebsiteContext);
 
     return (
         <div style={{ padding: '20px' }}>
@@ -15,37 +15,45 @@ function Tabela() {
                 onClick={() => novoObjeto()}>
                 Novo <i className="bi bi-file-earmark-plus"></i>
             </button>
-            {listaObjetos.length === 0 && <h1>Nenhuma categoria encontrada</h1>}
-            {listaObjetos.length > 0 && (
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col" style={{ textAlign: 'center' }}>Ações</th>
-                            <th scope="col">Id</th>
-                            <th scope="col">URL</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {listaObjetos.map(objeto => (
-                            <tr key={objeto.id}>
-                                <td align="center">
-                                    <button className="btn btn-info"
-                                        onClick={() =>{ editarObjeto(objeto.id) }}
-                                        data-bs-toggle="modal" data-bs-target="#modalEdicao">
-                                        <i className="bi bi-pencil-square"></i>
-                                    </button>
-                                    <button className="btn btn-danger" title="Remover"
-                                        onClick={() => { remover(objeto.id); }}>
-                                        <i className="bi bi-trash"></i>
-                                    </button>
-                                </td>
-                                <td>{objeto.id}</td>
-                                <td>{objeto.url}</td>
+            {(listaObjetos == null || listaObjetos.length === 0) && <h1>Nenhuma categoria encontrada</h1>}
+            {listaObjetos != null && listaObjetos.length > 0 && (
+                <div>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col" style={{ textAlign: 'center' }}>Ações</th>
+                                <th scope="col">Id</th>
+                                <th scope="col">URL</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {listaObjetos.map(objeto => (
+                                <tr key={objeto.id}>
+                                    <td align="center">
+                                        <button className="btn btn-info"
+                                            onClick={() =>{ editarObjeto(objeto.id) }}
+                                            data-bs-toggle="modal" data-bs-target="#modalEdicao">
+                                            <i className="bi bi-pencil-square"></i>
+                                        </button>
+                                        <button className="btn btn-danger" title="Remover"
+                                            onClick={() => { remover(objeto.id); }}>
+                                            <i className="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                    <td>{objeto.id}</td>
+                                    <td>{objeto.url}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             )}
+            <div className="pagination">
+                <button className="btn btn-primary" onClick={() =>{ previousPage(); }} disabled={page === 1}>Anterior</button>
+                <span style={{ padding: '10px' }} />
+                <button className="btn btn-primary" onClick={() =>{ nextPage(); }} disabled={listaObjetos == null || listaObjetos.length !== 10}>Próximo</button>
+            </div>
+            
         </div>
     )
 }
