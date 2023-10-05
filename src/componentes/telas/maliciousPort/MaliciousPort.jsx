7875749065035dbbf0bc7cecebbce8f2784d31d6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-05 17:01:21 -0300

    Update MaliciousPort.jsx

--- src/componentes/telas/maliciousPort/MaliciousPort.jsx
+++ src/componentes/telas/maliciousPort/MaliciousPort.jsx
@@ -12,12 +12,15 @@ import WithAuth from "../../../seguranca/WithAuth";
 function MaliciousPort(){
 
     let navigate = useNavigate();
+    const SIZE = 10;
 
     const [alerta, setAlerta] = useState({status : "", message : ""});
     const [listaObjetos, setListaObjetos] = useState([]);
     const [editar, setEditar] = useState(false);
     const [objeto, setObjeto] = useState({vulnarableBanners : ""});
     const [carregando, setCarregando] = useState(false);
+    const [page, setPage] = useState(1);
+
 
     const novoObjeto = () => {
         setEditar(false);
@@ -66,14 +69,16 @@ function MaliciousPort(){
 
     const recuperaMaliciousPorts = async () => {
         setCarregando(true);
-        let retornoAPI = await getAllMaliciousPortsAPI();
+        let retornoAPI = await getAllMaliciousPortsAPI(page, SIZE);
         if(retornoAPI === 0){
             setAlerta({ status: "Error", message: "Ops... você não tem acesso a essa página" });
             setCarregando(false);
             return;
         }
         if(retornoAPI == null){
-            setAlerta({ status: "No Content", message: "Não existem ports cadastradas" });
+            setAlerta({ status: "No Content", message: "Não existem sites cadastrados" });
+            setListaObjetos(retornoAPI);
+            setCarregando(false)
         }else{
             setListaObjetos(retornoAPI);
         }
@@ -93,6 +98,16 @@ function MaliciousPort(){
             navigate("/login", { replace: true });
         }
     }
+    
+    const nextPage = () => {
+        setPage(page => page + 1);
+    }
+
+    const previousPage = () => {
+        if (page > 1) {
+            setPage(page => page - 1);
+        }
+    }
 
     const handleChange = (e) => {
         const name = e.target.name;
@@ -102,13 +117,13 @@ function MaliciousPort(){
 
     useEffect(() => {
         recuperaMaliciousPorts();
-    },[]);
+    },[page]);
 
     return (
         <MaliciousPortContext.Provider value={{
             alerta, setAlerta, listaObjetos, remover,
             objeto, editar, acaoCadastrar, 
-            handleChange, novoObjeto, editarObjeto
+            handleChange, novoObjeto, editarObjeto, nextPage, previousPage, page
         }}>
             <Carregando carregando={carregando}>
             <Tabela/>
