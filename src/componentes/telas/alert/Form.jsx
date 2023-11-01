f84bc5943af3e441c0755cb75a353fccf80a902b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-01 14:46:32 -0300

    Update Form.jsx

--- src/componentes/telas/alert/Form.jsx
+++ src/componentes/telas/alert/Form.jsx
@@ -3,48 +3,16 @@ import Alerta from '../../comuns/Alerta';
 import AlertContext from './AlertContext';
 import CampoEntrada from '../../comuns/CampoEntrada';
 import Dialogo from '../../comuns/Dialogo';
-import { getAllImagesAPI } from '../../../servicos/services';
 
 function Form() {
-  const { objeto, handleChangeImg, handleChange, acaoCadastrar, alerta } = useContext(AlertContext);
-
-  const [imagensDisponiveis, setImagensDisponiveis] = useState([]);
-  
-  useEffect(() => {
-    const fetchImagensDisponiveis = async () => {
-      try {
-        setImagensDisponiveis(await getAllImagesAPI());
-      } catch (error) {
-        console.error('Erro ao obter imagens:', error);
-      }
-    };
-
-    fetchImagensDisponiveis();
-  }, []);
+  const { objeto, handleChange, acaoCadastrar, alerta } = useContext(AlertContext);
 
   return (
     <Dialogo id="modalEdicao" titulo="Alert" idformulario="formEdicao" acaoCadastrar={acaoCadastrar}>
       <Alerta alerta={alerta} />
       <CampoEntrada id="txtCodigo" label="Código" tipo="number" name="codigo" value={objeto.id} handlechange={handleChange} requerido={false} readonly={true} maximocaracteres={5} />
       <CampoEntrada id="txtPcId" label="pcId" tipo="text" name="pcId" value={objeto.pcid} handlechange={handleChange} requerido={true} readonly={false} textovalido="pcId OK" textoinvalido="Informe o pcId" maximocaracteres={40} />
-      <div className="form-group">
-        <label htmlFor="selectImageId">ImageId</label>
-        <select
-          id="selectImageId"
-          name="ImageId"
-          className="form-control"
-          value={objeto.image.id}
-          onChange={handleChangeImg}
-          required
-        >
-          {imagensDisponiveis.map(imagem => (
-            <option key={imagem.id} value={imagem.id}>
-              {imagem.productImg}
-            </option>
-          ))}
-        </select>
-      </div>
-
+      <CampoEntrada id="txtLog" label="log" tipo="text" name="log" value={objeto.log} handlechange={handleChange} requerido={true} readonly={false} textovalido="log OK" textoinvalido="Informe o log" maximocaracteres={40} />
       <CampoEntrada id="txtProcessos" label="Processos" tipo="text" name="processos" value={objeto.processos} handlechange={handleChange} requerido={true} readonly={false} textovalido="Processos OK" textoinvalido="Informe o Processos" maximocaracteres={40} />
     </Dialogo>
   );
