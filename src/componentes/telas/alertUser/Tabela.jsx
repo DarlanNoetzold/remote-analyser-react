baa676e28520c31b2bb9f63fd27a694000f0673a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-01 23:08:00 -0300

    Update Tabela.jsx

--- src/componentes/telas/alertUser/Tabela.jsx
+++ src/componentes/telas/alertUser/Tabela.jsx
@@ -1,14 +1,55 @@
 import React, { useContext, useState } from "react";
 import Alerta from '../../comuns/Alerta';
 import AlertUserContext from "./AlertUserContext";
+import CryptoJS from 'crypto-js';
 
 function Tabela() {
     const { alerta, listaObjetos, remover } = useContext(AlertUserContext);
-
+    
     const [mostrarTextoCompleto, setMostrarTextoCompleto] = useState(false);
+    const [alertData, setAlertData] = useState(null);
+    const [alertJSON, setAlertJSON] = useState("");
+    const [validationResult, setValidationResult] = useState("");
 
     const alternarExibicaoTexto = () => {
         setMostrarTextoCompleto(!mostrarTextoCompleto);
+    };
+
+    const generateHash = () => {
+        const jsonAlertData = JSON.stringify(alertData);
+        const hash = CryptoJS.SHA256(jsonAlertData).toString();
+        return hash;
+    };
+
+    const downloadAlertJSON = () => {
+        if (alertData) {
+            const hash = generateHash();
+            const jsonAlertData = JSON.stringify(alertData);
+
+            const blob = new Blob([jsonAlertData], { type: "application/json" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = `alert_${hash}.json`;
+            a.click();
+            URL.revokeObjectURL(url);
+        }
+    };
+
+    const handleValidation = (e) => {
+        e.preventDefault();
+
+        try {
+            const parsedAlert = JSON.parse(alertJSON);
+            const hash = generateHash();
+            if (hash === parsedAlert.hash) {
+                setValidationResult("Alerta válido.");
+            } else {
+                setValidationResult("Alerta inválido.");
+            }
+        } catch (error) {
+            setValidationResult("Erro na validação. Certifique-se de inserir um JSON válido.");
+        }
     };
 
     return (
@@ -37,15 +78,17 @@ function Tabela() {
                                     <img
                                         src={`data:image/jpeg;base64,${objeto.image}`}
                                         alt="Imagem"
-                                        style={{ maxWidth: '1500px', maxHeight: '1500px' }}
+                                        style={{ maxWidth: '900px', maxHeight: '600px' }}
                                     />
                                 </td>
                                 <td align="center">
-                                    <button className="btn btn-danger" title="Remover"
-                                        onClick={() => { remover(objeto.id); }}>
-                                        <i className="bi bi-trash"></i>
-                                    </button>
-                                </td>
+                                        <button className="btn btn-danger" title="Remover" onClick={() => { remover(objeto.id); }}>
+                                            <i className="bi bi-trash"></i>
+                                        </button>
+                                        <button className="btn btn-primary" title="Registrar Alerta" onClick={() => setAlertData(objeto)}>
+                                            Registrar Alerta
+                                        </button>
+                                    </td>
                                 <td>{objeto.id}</td>
                                 <td>{objeto.pcId}</td>
                                 <td>{objeto.log}</td>
@@ -76,6 +119,27 @@ function Tabela() {
                     </tbody>
                 </table>
             )}
+        <div>
+                <h2>Validar Alerta</h2>
+                <form onSubmit={handleValidation}>
+                    <label>
+                        Insira o JSON de Alerta:
+                        <textarea
+                            value={alertJSON}
+                            onChange={(e) => setAlertJSON(e.target.value)}
+                            rows={5}
+                            cols={50}
+                        />
+                    </label>
+                    <button type="submit">Validar</button>
+                </form>
+                {validationResult && (
+                    <div>
+                        <h3>Resultado da Validação:</h3>
+                        {validationResult}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
